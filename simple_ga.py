04d20911d77d933383370101d22cd01c4f92fc4e
Arman Arisman <arman.wu@gmail.com>
2023-05-07 15:58:47 +0700

    Update simple_ga.py

--- simple_ga.py
+++ simple_ga.py
@@ -1,4 +1,26 @@
 from random import randint
+
+#generate individual
 def individual(length, min, max):
     return[randint(min, max) for x in range(length)]
+#sample: print(individual(10, 0, 10))
 
+#generate population
+def population(count, length, min, max):
+    return[individual(length, min, max) for x in range(count)]
+#sample: print(population(10, 10, 0, 10))
+
+from operator import add
+from functools import reduce
+
+#generate individual
+def fitness(individual, target):
+    sum = reduce(add, individual, 0)
+    return abs(target-sum)
+""""
+sample:
+a = individual(10, 0, 10)
+print(a)
+print(reduce(add, a, 0))
+print(fitness(a, 100))
+"""
